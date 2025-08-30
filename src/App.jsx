07f4b53cbe723111feb463769b77cf1c0df59600
Yagami-light45 <harshithpasupuleti11@gmail.com>
2025-08-31 03:41:17 +0530

    navbar and basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Services,Subscribe,SuperQuality,CustomerReviews,Hero,PopularProducts,Footer,SpecialOffer } from "./sections"
+import Nav from "./components/nav"
 export default function App() {
   return (
-    
-      <h1 className="text-3xl font-bold underline pt-[100px]">
-        Hello world!
-      </h1>
+      <main className="relative">
+        <Nav></Nav>
+        <section className="xl:padding-1 wide:padding-r padding-b"><Hero></Hero></section>
+        <section className="padding"><PopularProducts></PopularProducts></section>
+        <section className="padding"><SuperQuality/></section>
+        <section className="padding"><Services/></section>
+        <section className="padding"><SpecialOffer/></section>
+        <section className="bg-pale-blue padding"><CustomerReviews/></section>
+        <section className="padding-x sm:py-32 py-16 w-full"><Subscribe/></section>
+        <section className="bg-black padding-x padding-t"><Footer/></section>
+      </main>
     
   )
 }
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import CustomerReviews from './CustomerReviews';
+import Services from './Services';
+import SuperQuality from './SuperQuality';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+import SpecialOffer from './SpecialOffer';
+
+export {
+    Hero,
+    PopularProducts,
+    CustomerReviews,
+    Services,
+    SuperQuality,
+    Subscribe,
+    Footer,
+    SpecialOffer
+};
