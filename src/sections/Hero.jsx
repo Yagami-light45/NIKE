126c76569f73b0df76277f6bc0373ba422340156
Yagami-light45 <harshithpasupuleti11@gmail.com>
2025-09-07 22:07:09 +0530

    PopularProducts

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
 import { shoes, statistics } from "../constants";
-import  Button  from "../components/Button";
+import  ShoeCard  from "../components/ShoeCard";
+import Button from "../components/Button";
 import { bigShoe1 } from "../assets/images";
 import { arrowRight } from "../assets/icons";
 
@@ -39,6 +40,29 @@ const Hero = () => {
               <p className='leading-7 font-montserrat text-slate-gray'>
                 {stat.label}
               </p>
+            </div>
+          ))}
+        </div>
+          
+      </div>
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
+        <img
+          src={bigShoeImg}
+          alt='shoe colletion'
+          width={610}
+          height={502}
+          className='object-contain relative z-10'
+        />
+
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
+          {shoes.map((image, index) => (
+            <div key={index}>
+              <ShoeCard
+                index={index}
+                imgURL={image}
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
+              />
             </div>
           ))}
         </div>
